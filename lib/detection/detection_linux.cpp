1e81669d5d855a20fffba9d7b73e63e9506e4a06
Yevhenii Nesterov <yevhenii.nesterov@jetbeep.com>
2019-10-24 15:51:52 +0300

    already connected devices detection

--- lib/detection/detection_linux.cpp
+++ lib/detection/detection_linux.cpp
@@ -6,7 +6,7 @@
 
 #include <thread>
 #include <unordered_map>
-
+#include <libudev.h>
 
 using namespace JetBeep;
 using namespace std;
@@ -15,13 +15,16 @@ class DeviceDetection::Impl {
   public:
 		Impl(DeviceDetectionCallback *callback);
 
-		void start();
-		void stop();
+		void startMonitoring();
+		void stopMonitoring();
+		void detectConnected();
 
 		virtual ~Impl();
 	private:
 		DeviceDetectionCallback *m_callback;
 		Logger m_log;
+		struct udev * udev;
+		
 		
 
 		//VidPid getVidPid(const io_service_t &service);
@@ -29,21 +32,82 @@ class DeviceDetection::Impl {
 
 };
 
+// DeviceDetection implementation
 
 DeviceDetection::Impl::Impl(DeviceDetectionCallback *callback) 
 	:m_callback(callback), m_log("detection") {
+		udev = udev_new();
+		if (udev == NULL) {
+			throw "Unable to initialize UDEV";
+		}
 }
 
 DeviceDetection::Impl::~Impl() {
-	stop();
+	stopMonitoring();
+	udev_unref(udev);
 }
 
-void DeviceDetection::Impl::stop() {
+void DeviceDetection::Impl::stopMonitoring() {
 	
 }
 
-void DeviceDetection::Impl::start() {
+void DeviceDetection::Impl::startMonitoring() {
 	
+}
+
+void DeviceDetection::Impl::detectConnected() {
+	struct udev_device *dev, *devParent;
+	struct udev_enumerate *enumerate;
+	struct udev_list_entry *devices, *deviceEntry, *attrsEntry;
+
+	enumerate = udev_enumerate_new(udev);
+	if (!enumerate) {
+		throw "UDEV: Unable to initialize UDEV enumerate";
+	}
+
+	udev_enumerate_add_match_subsystem(enumerate, "tty");
+	udev_enumerate_scan_devices(enumerate);
+
+	devices = udev_enumerate_get_list_entry(enumerate);
+	if (!devices) {
+		throw "UDEV: Unable to get devices list";
+	}
+
+	udev_list_entry_foreach(deviceEntry, devices) {
+		const char *path, *tmp, *vendorId, *productId;
+
+		path = udev_list_entry_get_name(deviceEntry);
+		dev = udev_device_new_from_syspath(udev, path);
+		if (!dev) continue;
+
+		struct udev_list_entry * attrsAvailable = udev_device_get_sysattr_list_entry(dev);
+
+		const char * devNode = udev_device_get_devnode(dev);
+		
+		if (!devNode) {
+			udev_device_unref(dev);
+			continue;
+		}
+
+		devParent = udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
+		if (devParent) {
+			vendorId = udev_device_get_sysattr_value(devParent,"idVendor");
+			productId = udev_device_get_sysattr_value(devParent,"idProduct");
+
+			VidPid deviceIds;
+			deviceIds.vid = strtol(vendorId, NULL, 16);
+			deviceIds.pid = strtol(productId, NULL, 16);
+			
+			if (DeviceDetection::isValidVidPid(deviceIds)) {
+				m_log.d() << "found JetBeep device: " << vendorId << ":" << productId << " path: "<< devNode << Logger::endl;
+			}
+			udev_device_unref(devParent);
+		}
+		
+		udev_device_unref(dev);
+	}
+	udev_enumerate_unref(enumerate);
+	udev_unref(udev);
 }
 
 
@@ -54,7 +118,10 @@ DeviceDetection::DeviceDetection(DeviceDetectionCallback callback)
 
 DeviceDetection::~DeviceDetection() {}
 
-void DeviceDetection::start() { m_impl->start(); }
-void DeviceDetection::stop() { m_impl->stop(); }
+void DeviceDetection::start() { 
+	m_impl->detectConnected();
+	m_impl->startMonitoring();
+}
+void DeviceDetection::stop() { m_impl->stopMonitoring(); }
 
 #endif
