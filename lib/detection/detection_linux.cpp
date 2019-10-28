7783e4d510d897cfdf4d86d21d514eed5b7c1222
Yevhenii Nesterov <yevhenii.nesterov@jetbeep.com>
2019-10-28 15:56:03 +0200

    Monitor add events fix

--- lib/detection/detection_linux.cpp
+++ lib/detection/detection_linux.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 #include <unordered_map>
 #include <libudev.h>
+#include <string>
 
 using namespace JetBeep;
 using namespace std;
@@ -30,7 +31,16 @@ private:
 	struct udev *udev;
 	bool isMonActive = false;
 
+	class UdevUSBDevice {
+		public:
+		string type;
+		string path;
+		string node;
+		string subsystem;
+	};
+
 	bool checkTTYParent(uDev *ttyDevice, DeviceCandidate &candidate);
+	bool checkDevVIDPID(uDev *ttyDevice, DeviceCandidate &candidate);
 };
 
 // DeviceDetection implementation
@@ -51,29 +61,43 @@ DeviceDetection::Impl::~Impl()
 	udev_unref(udev);
 }
 
-bool DeviceDetection::Impl::checkTTYParent(uDev *ttyDevice, DeviceCandidate &deviceCandidate)
+bool DeviceDetection::Impl::checkDevVIDPID(uDev *dev, DeviceCandidate &deviceCandidate)
 {
-	uDev *devParent = udev_device_get_parent_with_subsystem_devtype(ttyDevice, "usb", "usb_device");
-
-	if (!devParent)
+	if (!dev) {
 		return false;
+	}
+		
+	auto vendorId = udev_device_get_sysattr_value(dev, "idVendor");
+	auto productId = udev_device_get_sysattr_value(dev, "idProduct");
 
-	auto vendorId = udev_device_get_sysattr_value(devParent, "idVendor");
-	auto productId = udev_device_get_sysattr_value(devParent, "idProduct");
+	m_log.d() << "vid:pid "<<vendorId<< ":" <<productId << Logger::endl;
+
+
+	if (!vendorId || !productId) {
+		return false;
+	}
 
 	VidPid deviceIds;
 	deviceIds.vid = strtol(vendorId, NULL, 16);
 	deviceIds.pid = strtol(productId, NULL, 16);
 
-	udev_device_unref(devParent);
-
-	if (DeviceDetection::isValidVidPid(deviceIds))
-	{
+	if (DeviceDetection::isValidVidPid(deviceIds)) {
 		deviceCandidate.vid = deviceIds.vid;
 		deviceCandidate.pid = deviceIds.pid;
 		return true;
-	}
+	} 
 	return false;
+}
+
+bool DeviceDetection::Impl::checkTTYParent(uDev *ttyDevice, DeviceCandidate &deviceCandidate)
+{
+	uDev *devParent = udev_device_get_parent_with_subsystem_devtype(ttyDevice, "usb", "usb_device");
+
+	bool result = checkDevVIDPID(devParent, deviceCandidate);
+
+	udev_device_unref(devParent);
+
+	return result;
 }
 
 void DeviceDetection::Impl::stopMonitoring()
@@ -86,6 +110,7 @@ void DeviceDetection::Impl::startMonitoring()
 	if (isMonActive)
 		return;
 	isMonActive = true;
+	auto callback = *this->m_callback;
 
 	struct udev_enumerate *enumerate;
 	struct udev_list_entry *devices, *dev_list_entry;
@@ -93,15 +118,19 @@ void DeviceDetection::Impl::startMonitoring()
 	struct udev_monitor *mon;
 	int fd;
 
-	if (!udev)
+	if (!udev || callback == nullptr) {
 		return;
+	}
 
 	mon = udev_monitor_new_from_netlink(udev, "udev");
 	udev_monitor_filter_add_match_subsystem_devtype(mon, "tty", NULL);
+	udev_monitor_filter_add_match_subsystem_devtype(mon, "usb", "usb_device");
 	udev_monitor_enable_receiving(mon);
 	fd = udev_monitor_get_fd(mon);
 
 	m_log.d() << "Udev mon started" << Logger::endl;
+
+	UdevUSBDevice lastRemovedTTYdev;
 
 	while (isMonActive)
 	{
@@ -119,29 +148,53 @@ void DeviceDetection::Impl::startMonitoring()
 		if (ret > 0 && FD_ISSET(fd, &fds))
 		{
 			dev = udev_monitor_receive_device(mon);
-			if (dev)
-			{
-				string action(udev_device_get_action(dev));
-				m_log.d() << "device event: " << action << Logger::endl;
-				auto devNode = udev_device_get_devnode(dev);
-				DeviceCandidate candidate;
-				auto callback = *this->m_callback;
+			if (!dev) continue;
 
-				if (devNode && checkTTYParent(dev, candidate) && callback != nullptr)
-				{
-					candidate.path = string(devNode);
-					if (action == "add")
-					{
-						callback(ADDED, candidate);
-					}
-					else if (action == "remove")
-					{
-						callback(REMOVED, candidate);
-					}
-				}
+			string action(udev_device_get_action(dev));
 
-				udev_device_unref(dev);
+			m_log.d() << "Udev mon event: " << action << Logger::endl;
+
+			auto subsystem = udev_device_get_subsystem(dev);
+			auto devtype = udev_device_get_devtype(dev);
+			auto devpath = udev_device_get_devpath(dev);
+
+			if (!subsystem) {
+				continue;
 			}
+
+			DeviceCandidate candidate;
+
+			if (action == "add") {
+				auto devNode = udev_device_get_devnode(dev);
+				if (devNode && string(subsystem) == "tty" && checkTTYParent(dev, candidate)) {
+					callback(ADDED, candidate);
+				}
+			} else if (action == "remove") {
+				if (string(subsystem) == "usb" && checkDevVIDPID(dev, candidate)) {
+					//removed last
+					string usbDevPath(devpath ? devpath : "");
+					if (lastRemovedTTYdev.path.empty() 
+						// make sure last removed tty device is usb device child
+						// /devices/pci0000:00/0000:00:14.0/usb1/1-2/1-2.2 + /1-2.2:1.0/tty/ttyACM0
+						|| !(lastRemovedTTYdev.path.compare(0, usbDevPath.length(), usbDevPath) == 0)) {
+						m_log.d() << "JetBeep device removed - unknown tty path" << Logger::endl;
+					}
+
+					candidate.path = lastRemovedTTYdev.node;
+					lastRemovedTTYdev.path = "";
+					lastRemovedTTYdev.node = "";
+					callback(REMOVED, candidate);
+				} else if (string(subsystem) == "tty") {
+					//removed first
+					auto devNode = udev_device_get_devnode(dev);
+					lastRemovedTTYdev.node = devNode ? string(devNode) : "";
+					lastRemovedTTYdev.path = devpath ? string(devpath) : "";
+					m_log.d() << "tty " <<  devNode << Logger::endl;
+
+				}
+			}
+
+			udev_device_unref(dev);
 		}
 	}
 
