6a09dd6732c4b01cf5bfa61b29682b8d787b2587
Oleh Hordiichuk <oleh.hordiichuk@syllatech.com>
2019-11-18 13:14:55 +0200

    formatted main

--- examples/autodevice/main.cpp
+++ examples/autodevice/main.cpp
@@ -8,25 +8,25 @@ Logger l("main");
 
 int main() {
   Logger::coutEnabled = true;
-	Logger::level = LoggerLevel::verbose;
+  Logger::level = LoggerLevel::verbose;
   Cmd cmd;
 
   while (true) {
-		string input;
+    string input;
 
-		getline(cin, input);
-		input = Utils::toLowerCase(input);
-		auto splitted = Utils::splitString(input);
+    getline(cin, input);
+    input = Utils::toLowerCase(input);
+    auto splitted = Utils::splitString(input);
 
-		if (splitted.empty())	{
-			l.e() << "invalid input" << Logger::endl;
-			continue;
-		}
+    if (splitted.empty()) {
+      l.e() << "invalid input" << Logger::endl;
+      continue;
+    }
 
-		string command = splitted.at(0);
-		splitted.erase(splitted.begin());
-		
-		cmd.process(command, splitted);		
-	}
+    string command = splitted.at(0);
+    splitted.erase(splitted.begin());
+
+    cmd.process(command, splitted);
+  }
   return 0;
 }
