702e1342d0ba9c2a658aef2e2c5ca7694e6da3c7
Oleh Hordiichuk <oleh.hordiichuk@syllatech.com>
2020-01-25 22:31:26 +0200

    c-binding for autodevice

--- lib/libjetbeep.cpp
+++ lib/libjetbeep.cpp
@@ -131,3 +131,153 @@ JETBEEP_API jetbeep_error_t jetbeep_autodevice_create_payment(jetbeep_autodevice
   }
   return JETBEEP_NO_ERROR;
 }
+
+JETBEEP_API jetbeep_error_t jetbeep_autodevice_confirm_payment(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  try {
+    autodevice->confirmPayment();
+  } catch (const Errors::InvalidState&) {
+    return JETBEEP_ERROR_INVALID_STATE;
+  } catch (...) {
+    return JETBEEP_ERROR_IO;
+  }
+  return JETBEEP_NO_ERROR;
+}
+
+JETBEEP_API jetbeep_error_t jetbeep_autodevice_create_payment_token(jetbeep_autodevice_handle_t handle,
+                                                                    uint32_t amount,
+                                                                    const char* transaction_id,
+                                                                    size_t transaction_id_size,
+                                                                    jetbeep_autodevice_payment_token_result_cb callback,
+                                                                    const char* cashier_id,
+                                                                    size_t cashier_id_size,
+                                                                    const jetbeep_payment_metadata_t* metadata,
+                                                                    size_t metadata_size) {
+  auto autodevice = (AutoDevice*)handle;
+  try {
+    auto transactionId = string(transaction_id, transaction_id_size);
+    auto cashierId = string(cashier_id, cashier_id_size);
+    PaymentMetadata metaData;
+
+    for (int i = 0; i < metadata_size; ++i) {
+      auto key = string(metadata[i].key, metadata[i].key_size);
+      auto value = string(metadata[i].value, metadata[i].value_size);
+
+      metaData[key] = value;
+    }
+
+    autodevice->createPaymentToken(amount, transactionId, cashierId, metaData)
+      .then([callback](string token) { callback(JETBEEP_NO_ERROR, token.c_str(), token.size()); })
+      .catchError([callback](exception_ptr) { callback(JETBEEP_ERROR_IO, NULL, 0); });
+  } catch (const Errors::InvalidState&) {
+    return JETBEEP_ERROR_INVALID_STATE;
+  } catch (...) {
+    return JETBEEP_ERROR_IO;
+  }
+  return JETBEEP_NO_ERROR;
+}
+
+JETBEEP_API jetbeep_error_t jetbeep_autodevice_cancel_payment(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  try {
+    autodevice->cancelPayment();
+  } catch (const Errors::InvalidState&) {
+    return JETBEEP_ERROR_INVALID_STATE;
+  } catch (...) {
+    return JETBEEP_ERROR_IO;
+  }
+  return JETBEEP_NO_ERROR;
+}
+
+JETBEEP_API bool jetbeep_autodevice_is_mobile_connected(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  return autodevice->isMobileConnected();
+}
+
+JETBEEP_API const char* jetbeep_autodevice_version(jetbeep_autodevice_handle_t handle) {
+  const size_t MAX_STR_SIZE = 20;
+  static thread_local char version_str[20];
+  auto autodevice = (AutoDevice*)handle;
+  auto version = autodevice->version();
+
+  memset(version_str, 0, MAX_STR_SIZE);
+  memcpy(version_str, version.c_str(), version.size());
+  return version_str;
+}
+
+JETBEEP_API unsigned long jetbeep_autodevice_device_id(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  return autodevice->deviceId();
+}
+
+JETBEEP_API void* jetbeep_autodevice_get_opaque(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  return autodevice->opaque;
+}
+JETBEEP_API void jetbeep_autodevice_set_opaque(jetbeep_autodevice_handle_t handle, void* opaque) {
+  auto autodevice = (AutoDevice*)handle;
+  autodevice->opaque = opaque;
+}
+
+JETBEEP_API jetbeep_state_t jetbeep_autodevice_state(jetbeep_autodevice_handle_t handle) {
+  auto autodevice = (AutoDevice*)handle;
+  return (jetbeep_state_t)autodevice->state();
+}
+
+JETBEEP_API void jetbeep_autodevice_set_payment_error_callback(jetbeep_autodevice_handle_t handle,
+                                                               jetbeep_autodevice_payment_error_cb callback) {
+  auto autodevice = (AutoDevice*)handle;
+  autodevice->paymentErrorCallback = [callback](const PaymentError& error) {
+    jetbeep_error_t error_code;
+    switch (error) {
+    case PaymentError::discarded:
+      error_code = JETBEEP_ERROR_PAYMENT_DISCARDED;
+      break;
+    case PaymentError::invalidPin:
+      error_code = JETBEEP_ERROR_PAYMENT_INVALID_PIN;
+      break;
+    case PaymentError::network:
+      error_code = JETBEEP_ERROR_PAYMENT_NETWORK;
+      break;
+    case PaymentError::security:
+      error_code = JETBEEP_ERROR_PAYMENT_SECURITY;
+      break;
+    case PaymentError::server:
+      error_code = JETBEEP_ERROR_PAYMENT_SERVER;
+      break;
+    case PaymentError::timeout:
+      error_code = JETBEEP_ERROR_PAYMENT_TIMEOUT;
+      break;
+    case PaymentError::unknown:
+      error_code = JETBEEP_ERROR_PAYMENT_UNKNOWN;
+      break;
+    case PaymentError::withdrawal:
+      error_code = JETBEEP_ERROR_PAYMENT_WITHDRAWAL;
+      break;
+    default:
+      error_code = JETBEEP_ERROR_PAYMENT_UNKNOWN;
+      break;
+    }
+    callback(error_code);
+  };
+}
+
+JETBEEP_API void jetbeep_autodevice_set_state_callback(jetbeep_autodevice_handle_t handle, jetbeep_autodevice_state_cb callback) {
+  auto autodevice = (AutoDevice*)handle;
+  autodevice->stateCallback = [callback](AutoDeviceState state, exception_ptr) { callback((jetbeep_state_t)state); };
+}
+
+JETBEEP_API void jetbeep_autodevice_set_mobile_connected_callback(jetbeep_autodevice_handle_t handle,
+                                                                  jetbeep_autodevice_mobile_connected_cb callback) {
+  auto autodevice = (AutoDevice*)handle;
+  autodevice->mobileCallback = [callback](const SerialMobileEvent& event) {
+      switch (event) {
+          case SerialMobileEvent::connected:
+            callback(true);
+          break;
+          case SerialMobileEvent::disconnected:
+            callback(false);
+          break;
+      }
+  };
+}
--- lib/libjetbeep.h
+++ lib/libjetbeep.h
@@ -22,6 +22,14 @@ extern "C" {
 #define JETBEEP_NO_ERROR 0
 #define JETBEEP_ERROR_INVALID_STATE 1
 #define JETBEEP_ERROR_IO 2
+#define JETBEEP_ERROR_PAYMENT_NETWORK 100
+#define JETBEEP_ERROR_PAYMENT_TIMEOUT 101
+#define JETBEEP_ERROR_PAYMENT_SERVER 102
+#define JETBEEP_ERROR_PAYMENT_SECURITY 103
+#define JETBEEP_ERROR_PAYMENT_WITHDRAWAL 104
+#define JETBEEP_ERROR_PAYMENT_DISCARDED 105
+#define JETBEEP_ERROR_PAYMENT_UNKNOWN 106
+#define JETBEEP_ERROR_PAYMENT_INVALID_PIN 107
 
 #define JETBEEP_STATE_INVALID 0
 #define JETBEEP_STATE_FIRMWARE_VERSION_NOT_SUPPORTED 1
@@ -51,9 +59,9 @@ typedef struct {
 
 typedef void (*jetbeep_autodevice_barcode_result_cb)(jetbeep_error_t error, jetbeep_barcode_t* barcodes, size_t size);
 typedef void (*jetbeep_autodevice_payment_result_cb)(jetbeep_error_t error);
-typedef void (*jetbeep_autodevice_payment_token_result_cb)(const char* token, size_t size);
+typedef void (*jetbeep_autodevice_payment_token_result_cb)(jetbeep_error_t error, const char* token, size_t size);
 typedef void (*jetbeep_autodevice_payment_error_cb)(jetbeep_error_t error);
-typedef void (*jetbeep_autodevice_state_cb)(jetbeep_state_t state, jetbeep_error_t error);
+typedef void (*jetbeep_autodevice_state_cb)(jetbeep_state_t state);
 typedef void (*jetbeep_autodevice_mobile_connected_cb)(bool connected);
 
 JETBEEP_API jetbeep_autodevice_handle_t jetbeep_autodevice_new();
@@ -76,6 +84,7 @@ JETBEEP_API jetbeep_error_t jetbeep_autodevice_create_payment(jetbeep_autodevice
                                                               size_t metadata_size = 0);
 JETBEEP_API jetbeep_error_t jetbeep_autodevice_confirm_payment(jetbeep_autodevice_handle_t handle);
 JETBEEP_API jetbeep_error_t jetbeep_autodevice_create_payment_token(jetbeep_autodevice_handle_t handle,
+                                                                    uint32_t amount,
                                                                     const char* transaction_id,
                                                                     size_t transaction_id_size,
                                                                     jetbeep_autodevice_payment_token_result_cb callback,
@@ -87,11 +96,14 @@ JETBEEP_API jetbeep_error_t jetbeep_autodevice_cancel_payment(jetbeep_autodevice
 JETBEEP_API bool jetbeep_autodevice_is_mobile_connected(jetbeep_autodevice_handle_t handle);
 JETBEEP_API const char* jetbeep_autodevice_version(jetbeep_autodevice_handle_t handle);
 JETBEEP_API unsigned long jetbeep_autodevice_device_id(jetbeep_autodevice_handle_t handle);
-JETBEEP_API void* jetbeep_autodevice_opaque(jetbeep_autodevice_handle_t handle);
+JETBEEP_API void* jetbeep_autodevice_get_opaque(jetbeep_autodevice_handle_t handle);
+JETBEEP_API void jetbeep_autodevice_set_opaque(jetbeep_autodevice_handle_t handle, void* opaque);
 JETBEEP_API jetbeep_state_t jetbeep_autodevice_state(jetbeep_autodevice_handle_t handle);
-JETBEEP_API void jetbeep_autodevice_set_payment_error_callback(jetbeep_autodevice_handle_t handle, jetbeep_autodevice_payment_error_cb callback);
+JETBEEP_API void jetbeep_autodevice_set_payment_error_callback(jetbeep_autodevice_handle_t handle,
+                                                               jetbeep_autodevice_payment_error_cb callback);
 JETBEEP_API void jetbeep_autodevice_set_state_callback(jetbeep_autodevice_handle_t handle, jetbeep_autodevice_state_cb callback);
-JETBEEP_API void jetbeep_autodevice_set_mobile_connected_callback(jetbeep_autodevice_handle_t handle, jetbeep_autodevice_mobile_connected_cb callback);
+JETBEEP_API void jetbeep_autodevice_set_mobile_connected_callback(jetbeep_autodevice_handle_t handle,
+                                                                  jetbeep_autodevice_mobile_connected_cb callback);
 
 #ifdef __cplusplus
 }
