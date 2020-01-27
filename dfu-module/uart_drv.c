2020648ff0b63b98d19c6497f022ba4a2c6b33b7
Yevhenii Nesterov <yevhenii.nesterov@jetbeep.com>
2020-01-27 17:25:50 +0200

    nrfutil c port included in dfu module

---
+++ dfu-module/uart_drv.c
@@ -0,0 +1,26 @@
+#include "uart_drv.h"
+#include "logging.h"
+
+int uart_drv_open(uart_drv_t *p_uart)
+{
+	//TODO implement
+	return 0;
+}
+
+int uart_drv_close(uart_drv_t *p_uart)
+{
+	//TODO implement
+	return 0;
+}
+
+int uart_drv_send(uart_drv_t *p_uart, const uint8_t *pData, uint32_t nSize)
+{
+	//TODO implement
+	return 0;
+}
+
+int uart_drv_receive(uart_drv_t *p_uart, uint8_t *pData, uint32_t nSize, uint32_t *pSize)
+{
+	//TODO implement
+	return 0;
+}
